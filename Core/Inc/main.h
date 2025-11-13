91b8b186307d85bf0ad5bd73deed62abb4f3562c
Zachary Leyes <leyeszach@gmail.com>
2025-11-13 14:04:20 -0500

    added fatfs

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -73,7 +73,7 @@ void Error_Handler(void);
 #define SWO_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
-
+#define SD_SPI_HANDLE hspi1
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
